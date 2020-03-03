fc227b860d5c181179e8911d2bcde843f47f3d56
romncn <romncn@gmail.com>
2020-03-03 20:32:16 +0700

    show hero img

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends React.Component {
   state = {
     herodetails: []
   }
-  // console.log('response data', response)
   componentDidMount = () => {
     axios.get(
       'https://api.opendota.com/api/heroes'
@@ -32,19 +31,21 @@ class App extends React.Component {
               <span className="text-white m-2">FILTER</span>
               <select className="custom-select bg-secondary text-white" id="inlineFormCustomSelect">
                 <option selected>BY ROLE</option>
-                <option value="1">carry</option>
-                <option value="2">supp</option>
-                <option value="3">mid</option>
+                <option value="1">Carry</option>
+                <option value="2">Support</option>
+                <option value="3">Initiator</option>
+                <option value="3">Disabler</option>
+                <option value="3">Nuker</option>
               </select>
               <select className="custom-select bg-secondary text-white ml-2" id="inlineFormCustomSelect">
                 <option selected>BY ATTACK TYPE</option>
                 <option value="1">Melee</option>
-                <option value="2">Range</option>
+                <option value="2">Ranged</option>
               </select>
               <select className="custom-select bg-secondary text-white ml-2" id="inlineFormCustomSelect">
                 <option selected>Hero Name</option>
-                <option value="1">Melee</option>
-                <option value="2">Range</option>
+                <option value="1">Antimage</option>
+                <option value="2">Axe</option>
               </select>
 
             </div>
@@ -53,31 +54,56 @@ class App extends React.Component {
         <p />
         <div className="Content">
           <div className="heroes-list p-2">
+
             <div className="strength-hero">
               <h3 className="text-white">STRENGTH</h3>
-              <div className="row">
-                {this.state.herodetails.map((hero) => {
-                  if(hero.primary_attr == "str"){
-                    return <div className ="col-3 text-white">
-                      name : {hero.localized_name}
-                      <br/> type : {hero.primary_attr}
-                    </div>
-                  }
-                })}
+              <div className="container-fluid">
+                <div className="row">
+                  {this.state.herodetails.map((hero) => {
+                    if (hero.primary_attr == "str") {
+                      const localname = hero.name;
+                      const subname = localname.substring(14);
+                      return <div>
+                        <img className=" mr-1 ml-1 mt-2 " src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="48" height="70" />
+                      </div>
+                    }
+                  })}
+                </div>
               </div>
-
-
             </div>
             <div className="agility-hero">
               <h3 className="text-white">AGILITY</h3>
-                {/* <img src="http://cdn.dota2.com/apps/dota2/images/heroes/antimage_vert.jpg" ></img> */}
-
-
+              <div className="container-fluid">
+                <div className="row">
+                  {this.state.herodetails.map((hero) => {
+                    if (hero.primary_attr == "agi") {
+                      const localname = hero.name;
+                      const subname = localname.substring(14);
+                      return <div className="text-white">
+                        <img className=" mr-2 mt-2" src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="48" height="70" />
+                      </div>
+                    }
+                  })}
+                </div>
+              </div>
             </div>
             <div className="intelligence-hero">
               <h3 className="text-white">INTELLIGENCE</h3>
-
+              <div className="container-fluid">
+                <div className="row">
+                  {this.state.herodetails.map((hero) => {
+                    if (hero.primary_attr == "int") {
+                      const localname = hero.name;
+                      const subname = localname.substring(14);
+                      return <div className="text-white">
+                        <img className=" mr-2 mt-2" src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="48" height="70" />
+                      </div>
+                    }
+                  })}
+                </div>
+              </div>
             </div>
+
           </div>
         </div>
       </div>
