8cfdd16feb36488f98791f6368487d30afe8f74e
romncn <romncn@gmail.com>
2020-03-05 18:41:36 +0700

    [UPDATE]UI

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Filter = (props) => {
   return (
     <div>
       <div class="dropdown ml-1 mr-1">
-        <button class="btn filter-btn dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown"
+        <button class="btn filter-btn title-color dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false">
           {props.filter}
         </button>
@@ -55,8 +55,8 @@ class App extends React.Component {
   Alert = (hero) => {
     swal.fire({
       imageUrl: 'http://cdn.dota2.com/apps/dota2/images/heroes/' + hero.name.substring(14) + '_full.png',
-      imageWidth: 400,
-      imageHeight: 200,
+      imageWidth: 350,
+      imageHeight: 225,
       title: hero.localized_name,
       text: hero.attack_type,
       footer: hero.roles,
@@ -79,8 +79,8 @@ class App extends React.Component {
 
   FilterRole = (type) => {
     let role = this.state.herodetails
-    if (type !== 'All') 
-    role = this.state.herodetails.filter((hero) => hero.roles.includes(type) === true)
+    if (type !== 'All')
+      role = this.state.herodetails.filter((hero) => hero.roles.includes(type) === true)
     this.setState({
       heroShow: role,
       filter: { type: type }
@@ -88,8 +88,8 @@ class App extends React.Component {
   }
   FilterAtk = (type) => {
     let atk = this.state.herodetails
-    if (type !== 'All') 
-    atk = this.state.herodetails.filter((hero) => hero.attack_type.includes(type) === true)
+    if (type !== 'All')
+      atk = this.state.herodetails.filter((hero) => hero.attack_type.includes(type) === true)
     this.setState({
       heroShow: atk,
       filter: { type: type }
@@ -97,8 +97,8 @@ class App extends React.Component {
   }
   FilterName = (type) => {
     let name = this.state.herodetails
-    if (type !== 'All') 
-    name = this.state.herodetails.filter((hero) => hero.localized_name.includes(type) === true)
+    if (type !== 'All')
+      name = this.state.herodetails.filter((hero) => hero.localized_name.includes(type) === true)
     this.setState({
       heroShow: name,
       filter: { type: type }
@@ -108,25 +108,25 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        
-        <div className="fliter-block p-2">
-          <div className="container">
+        <div className="text-center">
+          <div className="fliter-block title-color p-2">
+            <div className="container">
               <div className="row">
-                <span className="text-white m-2">FILTER</span>
-                <Filter filter={'Role'} types={this.state.heroRole} filterFunc={this.FilterRole} />
-                <Filter filter={'Attack Type'} types={this.state.heroAtkType} filterFunc={this.FilterAtk} />
-                <Filter filter={'Hero Name'} types={this.state.heroName} filterFunc={this.FilterName} />
+                <span className="m-2">FILTER</span>
+                <Filter filter={'BY ROLE'} types={this.state.heroRole} filterFunc={this.FilterRole} />
+                <Filter filter={'BY ATTACK TYPE'} types={this.state.heroAtkType} filterFunc={this.FilterAtk} />
+                <Filter filter={'HERO NAME'} types={this.state.heroName} filterFunc={this.FilterName} />
               </div>
-          
             </div>
+          </div>
         </div>
         <p />
         <div className="Content">
           <div className="heroes-list p-2">
 
             {/* show strength hero */}
-            <div className="strength-hero">
-              <h3 className="text-white">STRENGTH</h3>
+            <div className="strength-hero pb-2 pl-1">
+              <h5 className="title-color">STRENGTH</h5>
               <div className="container-fluid">
                 <div className="row">
 
@@ -134,8 +134,8 @@ class App extends React.Component {
                     if (hero.primary_attr == "str") {
                       const subname = hero.name.substring(14);
                       return <div>
-                        <img onClick={() => this.Alert(hero)} cursor="point" className="hero-img mr-1 ml-1 mt-2 "
-                          src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="50" height="70" />
+                        <img onClick={() => this.Alert(hero)} cursor="point" className="hero-img mr-1 ml-1 "
+                          src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="55" height="80" />
                       </div>
                     }
                   })}
@@ -145,16 +145,16 @@ class App extends React.Component {
             </div>
 
             {/* show agility hero */}
-            <div className="agility-hero">
-              <h3 className="text-white">AGILITY</h3>
+            <div className="agility-hero pb-2 pl-1">
+              <h5 className="title-color">AGILITY</h5>
               <div className="container-fluid">
                 <div className="row">
                   {this.state.heroShow.map((hero) => {
                     if (hero.primary_attr == "agi") {
                       const subname = hero.name.substring(14);
                       return <div>
-                        <img onClick={() => this.Alert(hero)} className="hero-img mr-2 mt-2"
-                          src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="48" height="70" />
+                        <img onClick={() => this.Alert(hero)} className="hero-img mr-2 "
+                          src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="55" height="80" />
                       </div>
                     }
                   })}
@@ -164,16 +164,16 @@ class App extends React.Component {
             </div>
 
             {/* show intelligence hero */}
-            <div className="intelligence-hero">
-              <h3 className="text-white">INTELLIGENCE</h3>
+            <div className="intelligence-hero pb-2 pl-1">
+              <h5 className="title-color">INTELLIGENCE</h5>
               <div className="container-fluid">
                 <div className="row">
                   {this.state.heroShow.map((hero) => {
                     if (hero.primary_attr == "int") {
                       const subname = hero.name.substring(14);
                       return <div>
-                        <img onClick={() => this.Alert(hero)} className="hero-img mr-2 mt-2"
-                          src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="48" height="70" />
+                        <img onClick={() => this.Alert(hero)} className="hero-img mr-2"
+                          src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + subname + '_vert.jpg'} width="55" height="80" />
                       </div>
                     }
                   })}
